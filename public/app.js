58e4be745087982bd7143455c56277b389587ffe
Dan O'Reilly <dangoreilly@gmail.com>
2022-12-08 00:11:50 +0000

    initial commit

---
+++ public/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUNmTz5VnBiNdoeufmLPiohtI6_VIhZsk",
+  authDomain: "nebb-fire.firebaseapp.com",
+  projectId: "nebb-fire",
+  storageBucket: "nebb-fire.appspot.com",
+  messagingSenderId: "915351982313",
+  appId: "1:915351982313:web:1d0a296bc71879dbdb6078"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
